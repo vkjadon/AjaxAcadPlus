2f91c3b2ed364044b1d7fc76746ba741569dba5f
vkjadon <vijay.jadon@gmail.com>
2021-04-21 17:46:05 +0530

    r

--- module/registration/registrationSql.php
+++ module/registration/registrationSql.php
@@ -79,7 +79,7 @@ if (isset($_POST['action'])) {
         $sql = "insert into $tn_rc (student_id, class_id, rc_date, submit_id) values('$id[$i]','$classId','$submit_date','$myId')";
         $result = $conn->query($sql);
         for ($j = 0; $j < count($tl); $j++) {
-          $sql = "select * from $tn_rs where student_id='$id[$i]' and tl_id='$tl[$j]' and rs_status='0'";
+          $sql = "select * from $tn_rs where student_id='$id[$i]' and tl_id='$tl[$j]'";
           if (!$conn->query($sql)->num_rows) {
             $sql = "insert into $tn_rs (student_id, tl_id, update_id) values('$id[$i]','$tl[$j]','$myId')";
             $result = $conn->query($sql);
@@ -113,60 +113,58 @@ if (isset($_POST['action'])) {
     $student_id = $_POST['stdId'];
     $class_id = $_POST['classId'];
     //echo "Student - ".$student_id;
-    
+    $totalCredit=0;
+    $sql = "select student_name from student where student_id='$student_id'";
+    $student_name = getFieldValue($conn, 'student_name', $sql);
+    echo '<h5 class="text-center">' . $student_name . '[' . $student_id . ']</h5>';
+    echo '<table class="table list-table-xs mb-0">';
+    echo '<tr><th>#</th><th></th><th>Id</th><th>Subject</th><th>Code</th><th>Cr</th><th>Class</th></tr>';
+
+
     $sql = "select tl.tl_id, sb.* from $tn_tlg tlg, $tn_tl tl, subject sb where tlg.tlg_id=tl.tlg_id and tlg.subject_id=sb.subject_id and tlg.class_id='$class_id'";
     $result = $conn->query($sql);
     if (!$result) echo $conn->error;
     $i = 0;
     while ($rowArray = $result->fetch_assoc()) {
-      $tl[$i] = $rowArray["tl_id"];
-      $subject_id[$i]= $rowArray["subject_id"];
-      //$credit= $rowArray["credit"];
-      $i++;
-    }
-
-    $sql = "select student_name from student where student_id='$student_id'";
-    $student_name = getFieldValue($conn, 'student_name', $sql);
-    $credit = 0;
-    echo '<h5 class="text-center">' . $student_name . '[' . $student_id . ']</h5>';
-    echo '<table class="table list-table-xs mb-0">';
-    echo '<tr><th>#</th><th></th><th>Id</th><th>Subject</th><th>Code</th><th>Cr</th><th>Class</th></tr>';
-    for ($i = 0; $i < count($tl); $i++) {
-      $rs_tl = $tl[$i];
+      $rs_tl = $rowArray["tl_id"];
+      $subject_id= $rowArray["subject_id"];
+      $subject_credit= $rowArray["subject_credit"];
+    
       //echo $rs_tl;
-      $sql = "select * from $tn_rs where student_id='$student_id' and tl_id='$rs_tl'";
+      $sql = "select * from $tn_rs where student_id='$student_id' and tl_id='$rs_tl' and rs_status='0'";
       $check_status = getFieldValue($conn, 'rs_id', $sql);
       echo '<tr>';
       echo '<td>' . ($i + 1) . '</td>';
       if ($check_status == "") echo '<td><input type="checkbox" class="stdsubCheckbox" data-std="' . $student_id . '" value="' . $rs_tl . '"></td>';
       else {
         echo '<td><input type="checkbox" class="stdsubCheckbox" data-std="' . $student_id . '" value="' . $rs_tl . '" checked></td>';
-        //$credit += $array["data"][$i]["credit"];
+        $totalCredit += $subject_credit;
       }
-      echo '<td>' . $subject_id[$i] . '</td>';
+      echo '<td>' . $subject_id . '</td>';
       echo '<td>' . $rowArray["subject_name"] . '</td>';
+      echo '<td>' . $rowArray["subject_code"] . '</td>';
+      echo '<td>' . $subject_credit . '</td>';
       if ($check_status == "") echo '<td>--</td>';
       else {
-        $sql = "select * from $tn_rs where student_id='$student_id' and tl_id='$rs_tl'";
-        $rs_class = getFieldValue($conn, 'class_id', $sql);
-        $class = getField($conn, $rs_class, "class", "class_id", "class_name");
-        $group = getField($conn, $check_status, $tn_rs, "rs_id", "tl_group");
-        echo '<td><button class="btn btn-secondary btn-square-sm updateClassButton" value="' . $check_status . '" data-class="' . $class . '" data-group="' . $group . '" data-subject="' . $rs_tl . '">' . $class . '</button></td>';
+        
+        echo '<td><button class="btn btn-secondary btn-square-sm updateClassButton" value="' . $check_status . '">Update</button></td>';
+        echo '<td><button class="btn btn-secondary btn-square-sm updateClassButton" value="' . $check_status . '">Group</button></td>';
       }
       echo '</tr>';
+      $i++;
     }
     echo '</table>';
-    echo '<h5>Credits Registered : ' . $credit . '</h5>';
+    echo '<h5>Credits Registered : ' . $totalCredit . '</h5>';
   } elseif ($_POST['action'] == 'updateSub') {
     $student_id = $_POST['stdId'];
-    $subject_id = $_POST['subId'];
+    $tl_id = $_POST['subId'];
     $status = $_POST['status'];
     if ($status == "false") {
       echo "False Detected ";
-      $sql = "update $tn_rs set rs_status='1' where student_id='$student_id' and subject_id='$subject_id'";
+      $sql = "update $tn_rs set rs_status='1' where student_id='$student_id' and tl_id='$tl_id'";
     } else {
       echo "True Detected ";
-      $sql = "update $tn_rs set rs_status='0' where student_id='$student_id' and subject_id='$subject_id'";
+      $sql = "update $tn_rs set rs_status='0' where student_id='$student_id' and tl_id='$tl_id'";
     }
     $conn->query($sql);
   }
